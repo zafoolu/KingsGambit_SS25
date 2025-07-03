a0c777b3b7d625bc4601014d3db0535c4d24411f
zafoolu <sologkevin@gmail.com>
2025-07-03 11:43:29 +0200

    fix2

--- Assets/Scripts/Authoring/UnitMoverAuthoring.cs
+++ Assets/Scripts/Authoring/UnitMoverAuthoring.cs
@@ -2,27 +2,39 @@ using Unity.Entities;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class UnitMoverAuthoring : MonoBehaviour {
+public class UnitMoverAuthoring : MonoBehaviour
+{
 
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
-    
-    [Header("Collision Avoidance")]
-    public float personalSpace = 1.5f;       // Minimaler Abstand zu anderen Einheiten
-    public float avoidanceWeight = 0.7f;     // Wie stark die Einheit ausweicht (0-1)
 
-    public class Baker : Baker<UnitMoverAuthoring> {
+    public float moveSpeed;
+    public float rotationSpeed;
 
-        public override void Bake(UnitMoverAuthoring authoring) {
+
+    public class Baker : Baker<UnitMoverAuthoring>
+    {
+
+        public override void Bake(UnitMoverAuthoring authoring)
+        {
             Entity entity = GetEntity(TransformUsageFlags.Dynamic);
-            AddComponent(entity, new UnitMover {
+            AddComponent(entity, new UnitMover
+            {
                 moveSpeed = authoring.moveSpeed,
                 rotationSpeed = authoring.rotationSpeed,
-                personalSpace = authoring.personalSpace,
-                avoidanceWeight = authoring.avoidanceWeight,
-                avoidanceDirection = float3.zero,
-                isMoving = false
             });
         }
+
     }
+
+}
+
+public struct UnitMover : IComponentData
+{
+
+
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float3 targetPosition;
+    public bool isMoving;
+
+
 }
