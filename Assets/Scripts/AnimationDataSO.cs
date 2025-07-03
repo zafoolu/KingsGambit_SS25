c929ead4c8add52848134598c49de6353655921c
Gravemanxo <yasin.spahlinger@gmail.com>
2025-07-03 15:40:11 +0200

    Rook fertig

--- Assets/Scripts/AnimationDataSO.cs
+++ Assets/Scripts/AnimationDataSO.cs
@@ -19,7 +19,8 @@ public class AnimationDataSO : ScriptableObject {
         CarraraKnightWalk,
         CarraraKnightIdle,
         CarraraQueenWalk,
-        CarraraQueenIdle,
+        CarraraRookWalk,
+        CarraraRookIdle,
     }
 
 
