9c02a4d23da4728a5aa46d39dd4b85780433e41b
Gravemanxo <yasin.spahlinger@gmail.com>
2025-07-16 22:55:00 +0200

    nulll

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -2,20 +2,59 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenuUI : MonoBehaviour {
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button newCampaignButton;
+    [SerializeField] private Button battlesButton;
+    [SerializeField] private GameObject skirmishMenuUI;
 
+    private void Awake()
+    {
+        Debug.Log("MainMenuUI Awake");
 
-    [SerializeField] private Button playButton;
-    [SerializeField] private Button quitButton;
+        // Skirmish-Menü initial ausblenden
+        if (skirmishMenuUI != null)
+        {
+            skirmishMenuUI.SetActive(false);
+        }
+        else
+        {
+            Debug.LogWarning("SkirmishMenuUI ist nicht zugewiesen!");
+        }
 
+        // Kampagnen-Button
+        if (newCampaignButton != null)
+        {
+            newCampaignButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Campaign clicked");
+                SceneManager.LoadScene(1); // Stelle sicher, dass Szene 1 existiert
+            });
+        }
+        else
+        {
+            Debug.LogWarning("NewCampaignButton ist nicht zugewiesen!");
+        }
 
-    private void Awake() {
-        playButton.onClick.AddListener(() => {
-            SceneManager.LoadScene(1);
-        });
-        quitButton.onClick.AddListener(() => {
-            Application.Quit();
-        });
+        // Battles-Button
+        if (battlesButton != null)
+        {
+            battlesButton.onClick.AddListener(() =>
+            {
+                Debug.Log("Battles clicked");
+
+                if (skirmishMenuUI != null)
+                {
+                    bool isActive = skirmishMenuUI.activeSelf;
+                    Debug.Log("SkirmishMenuUI is currently " + (isActive ? "active" : "inactive"));
+                    skirmishMenuUI.SetActive(!isActive);
+                    Debug.Log("SkirmishMenuUI is now " + (!isActive ? "active" : "inactive"));
+                }
+            });
+        }
+        else
+        {
+            Debug.LogWarning("BattlesButton ist nicht zugewiesen!");
+        }
     }
-
 }
